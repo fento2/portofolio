bf1bda48c2736b0f5ed741a35041ed855ed956b7
fento2 <ftonrate91@gmail.com>
2025-06-21 16:28:19 +0700

    init

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+function Navbar(){
+    return(
+        <div>
+            <nav className="bg-black text-white">
+                <ul>
+                    <li>FENDRY TONRATE</li>
+                    <li>ABOUT</li>
+                    <li>EXPERIENCE</li>
+                    <li>PROJECTS</li>
+                    <li>CONTACT</li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+export default Navbar;
